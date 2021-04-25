f1f177c2ce86e3d4ffe9e4a2b8ca128d1ae7656e
MarkZaretskiy <mark.zaretskiy@phystech.edu>
2021-04-25 08:48:08 +0300

    relocate dataset

--- src/model.py
+++ src/model.py
@@ -481,7 +481,7 @@ class Killer:
 
     def predict(self, df_test):
         
-        df_train = pd.read_csv("../data/train_set_labels0_sub.csv")
+        df_train = pd.read_csv("./train_set_labels0_sub.csv")
         y = df_train['mean_growth_PH']
         #X_train = get_X(df_train)
         # X = np.load("/home/markzaretckii/Desktop/bhp/optimal-ph/data/sequence_features.npy")
@@ -504,5 +504,14 @@ class Killer:
 
         return predictions
 
+from sklearn.ensemble import ExtraTreesRegressor
+
+ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse',
+max_depth=None, max_features='auto', max_leaf_nodes=None,
+max_samples=None, min_impurity_decrease=0.0,
+min_impurity_split=None, min_samples_leaf=1,
+min_samples_split=2, min_weight_fraction_leaf=0.0,
+n_estimators=100, n_jobs=-1, oob_score=False,
+random_state=5910, verbose=0, warm_start=False)
 
 
